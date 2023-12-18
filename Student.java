472a2f963236a9d3e8df56e6e86e0d025493859d
Dinara E <152121228+00-slats@users.noreply.github.com>
2023-12-18 13:49:26 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,19 @@
+// Student.java
+
+public class Student {
+    private int studentId;
+    private String name;
+    private int age;
+
+    public Student(int studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
